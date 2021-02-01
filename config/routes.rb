9a7ceedbe6e7470f9fe78502e761495c55b7c2b0
yuanxizhang <yuanxi.zhang@gmail.com>
2021-02-01 02:17:39 -0800

    Add users_controller.rb, update Application_controller.rb with helper_methos, update route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,14 @@ Rails.application.routes.draw do
       end
 
       resources :users, only: [:create, :show, :index]
+
+      post '/users',         to: 'users#create'
+      get '/users/:user_id', to: 'users#show'
+      get '/users',          to: 'users#index'
+
+      post '/login', to: 'sessions#create'
+      delete '/logout', to: 'sessions#destroy'
+      get '/logged_in', to: 'sessions#is_logged_in?'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
