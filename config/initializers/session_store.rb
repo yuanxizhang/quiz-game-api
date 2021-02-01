4515209e4a6fd4b72e58ca16916efe2179f20650
yuanxizhang <yuanxi.zhang@gmail.com>
2021-02-01 02:03:06 -0800

    Generate User model user.rb, create users table, update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,7 @@
-if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, {
+Rails.application.config.session_store :cookie_store, {
 		  :key => '_quiz-game',
 		  :domain => :all,
 		  :same_site => :none,
 		  :secure => :true,
 		  :tld_length => 2
 		}
-else
-    Rails.application.config.session_store :cookie_store, key: '_quiz-game'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_073206) do
+ActiveRecord::Schema.define(version: 2021_02_01_095114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,14 @@ ActiveRecord::Schema.define(version: 2020_12_13_073206) do
 
   create_table "tests", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
